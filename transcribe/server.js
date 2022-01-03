83b7efdaddbf135b99bca9ca035330679bdaa56e
Hammad Siddiqui <hammadsiddiqui@Hammads-MacBook-Air.local>
2022-01-03 17:59:32 +0300

    almost there

--- transcribe/server.js
+++ transcribe/server.js
@@ -2,20 +2,36 @@ var express = require('express');    //Express Web Server
 var busboy = require('connect-busboy'); //middleware for form/file upload
 var path = require('path');     //used for file path
 var fs = require('fs-extra');       //File System - for file manipulation
-
+const multer  = require('multer')
 var app = express();
 app.use(busboy());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const speech = require('@google-cloud/speech');
+
 
 app.get('/', function (req,res){
     res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
-/* ========================================================== 
-Create a Route (/upload) to handle the Form submission 
-(handle POST requests to /upload)
-Express v4  Route definition
-============================================================ */
+
+/**
+ * @desc upload a file to the server so that you can later <display> it.
+ * @param  {obj} file
+ * @param {string} type
+ * @return bool - success or failure
+ */
+
+
+
+// const upload = multer({
+//     dest: './audio',
+//     //fileFilter,
+// });
+//
+// app.post('/upload', upload.single('file'));
+
+
 app.route('/upload')
     .post(function (req, res, next) {
 
@@ -25,14 +41,44 @@ app.route('/upload')
             console.log("Uploading: " + filename);
 
             //Path where image will be uploaded
-            fstream = fs.createWriteStream(__dirname + '/audio/' + filename);
+            fstream = fs.createWriteStream(__dirname + '/audio/' + filename.filename);
             file.pipe(fstream);
             fstream.on('close', function () {
-                console.log("Upload Finished of " + filename);
-                res.redirect('back');           //where to go next
+                console.log("Upload Finished of " + filename.filename);
+                res.send('File uploaded');           //where to go next
             });
         });
     });
+
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+async function transcribe() {
+    // The path to the remote LINEAR16 file
+    const gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
+
+    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+    const audio = {
+        uri: gcsUri,
+    };
+    const config = {
+        encoding: 'MP3',
+        sampleRateHertz: 22050,
+        languageCode: 'en-US',
+    };
+    const request = {
+        audio: audio,
+        config: config,
+    };
+
+    // Detects speech in the audio file
+    const [response] = await client.recognize(request);
+    const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+    console.log(`Transcription: ${transcription}`);
+}
 
 var server = app.listen(3030, function() {
     console.log('Listening on port %d', server.address().port);
